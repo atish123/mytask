9af6706410c2277e41486cbcc7823a0f9df90bf0
atish <aatish93maske@gmail.com>
2015-03-12 16:14:52 +0530

    Changes in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = User.create! :email => 'test@test.com', :password => 'password', :password_confirmation => 'password', :admin => 'true'
+user = User.create! :username => 'admin' :email => 'test@test.com', :password => 'password', :password_confirmation => 'password', :admin => 'true'
